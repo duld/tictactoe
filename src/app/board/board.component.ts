eb8fc1f21db80ab80f52a8664ae98ea59d88a84b
Jeffrey McCrea <duldren@gmail.com>
2019-10-20 13:08:31 -0700

    Update game logic

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -8,19 +8,57 @@ import { SquareComponent } from '../square/square.component';
 })
 export class BoardComponent implements OnInit {
 
-  gameSquares: SquareComponent[] = [];
+  gameSquares: any[];
+  xIsNext: boolean;
+  winner: string;
 
   constructor() {
-   }
+  }
 
   ngOnInit() {
     this.newGame();
   }
 
   newGame() {
-    for (let i = 0; i < 9; i++) {
-      this.gameSquares.push( new SquareComponent());
+    console.log('new game');
+    this.gameSquares = Array(9).fill(null);
+    this.winner = null;
+    this.xIsNext = true;
+  }
+
+  get player() {
+    return this.xIsNext ? 'X' : 'O';
+  }
+
+  test() {
+    console.log(this.gameSquares);
+  }
+
+  makeMove(idx: number) {
+    if (!this.gameSquares[idx] && !this.winner) {
+      this.gameSquares.splice(idx, 1, this.player);
+      this.winner = this.calculateWinner();
     }
+    this.xIsNext = !this.xIsNext;
+  }
+
+  calculateWinner() {
+    const lines = [
+      [0, 1, 2], [0, 3, 6], [0, 4, 8],
+      [3, 4, 5], [6, 7, 8], [1, 4, 7],
+      [2, 5, 8], [2, 4, 6]
+    ];
+
+    for (const line of lines) {
+      if (
+        this.gameSquares[line[0]] === this.player &&
+        this.gameSquares[line[1]] === this.player &&
+        this.gameSquares[line[2]] === this.player
+      ) {
+        return this.player;
+      }
+    }
+    return null;
   }
 
 }
